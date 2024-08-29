97acbdeb855fa458d30d519ed17aa5fc1210d7af
Emeka-techDev <gilbertgenye@gmail.com>
2024-08-29 10:28:13 +0100

    fixed user-profile

--- app/Http/Controllers/ChangePasswordController.php
+++ app/Http/Controllers/ChangePasswordController.php
@@ -5,18 +5,30 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\ChangePasswordRequest;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class ChangePasswordController extends Controller
 {
     public function changePassword(ChangePasswordRequest $request) 
     {
         
-
         try {
             $user =  $request->user();
-            $user->password = $request['password'];
+            
+            if (!Hash::check($request['old_password'], $user->password)) {
+                // throw error that old password dont match
+                throw ValidationException::withMessages([
+                    'old_password' => ['Old password is incorrect.']
+                ]);
+            }
+
+            // Update the user's password
+            $user->password = Hash::make($request['password']);
             $user->save();
+
         } catch (\Exception $exception){
+            
             return response()->json(['error' => true, 'message' => $exception->getMessage()], 500);
         }
 
