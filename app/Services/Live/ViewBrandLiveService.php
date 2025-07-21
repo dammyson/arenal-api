9aa3972cbfbd5660afa282f19307deac83fe9de4
Emeka-techDev <gilbertgenye@gmail.com>
2025-07-21 17:08:14 +0100

    live count

--- app/Services/Live/ViewBrandLiveService.php
+++ app/Services/Live/ViewBrandLiveService.php
@@ -8,6 +8,7 @@ use App\Models\BrandDetail;
 use App\Services\BaseServiceInterface;
 use App\Http\Requests\Live\StoreLiveRequest;
 use App\Http\Requests\User\BrandStoreRequest;
+use App\Models\LiveTicket;
 
 class ViewBrandLiveService implements BaseServiceInterface{
     protected $brandId;
@@ -19,9 +20,14 @@ class ViewBrandLiveService implements BaseServiceInterface{
 
     public function run() {
         try {
-            $live = Live::where("brand_id", $this->brandId)->get();
+            $live = Live::where("brand_id", $this->brandId)->first();
 
-            return $live;
+            $liveCount = LiveTicket::where("live_id", $live->id)->where("is_live", true)->count();
+
+            return [
+                "live" => $live, 
+                "live_count" => $liveCount
+            ];
 
         } catch(\Throwable $e) {
             
