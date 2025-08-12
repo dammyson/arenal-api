e68cfc60fe273e8598f0cceae0734350cca7b496
Emeka-techDev <gilbertgenye@gmail.com>
2025-08-12 11:47:01 +0100

    achievements

--- app/Http/Controllers/PrizeController.php
+++ app/Http/Controllers/PrizeController.php
@@ -5,21 +5,22 @@ namespace App\Http\Controllers;
 use App\Models\Brand;
 use App\Models\Prize;
 use Illuminate\Http\Request;
+use App\Models\BrandAudienceReward;
+use App\Models\AudiencePrizeDelivery;
 use App\Http\Controllers\BaseController;
-use App\Http\Requests\Prize\PrizeDeliveryRequest;
-use App\Http\Requests\Prize\UpdatePrizeDeliveryRequest;
 use App\Services\Brand\StoreBrandService;
 use App\Services\Prize\StorePrizeService;
 use App\Services\Prize\GetBrandPrizeService;
 use App\Http\Requests\User\BrandStoreRequest;
 use App\Http\Requests\User\PrizeStoreRequest;
-use App\Models\AudiencePrizeDelivery;
-use App\Models\BrandAudienceReward;
 use App\Services\Prize\GetBrandBadgesService;
 use App\Services\Prize\GetBrandPrizeUserService;
+use App\Http\Requests\Prize\PrizeDeliveryRequest;
 use App\Services\Prize\RedeemUserBrandPrizeService;
 use App\Services\Point\GetAudienceBrandPointService;
 use App\Services\Prize\GetBrandAudienceBadgeService;
+use App\Http\Requests\Prize\UpdatePrizeDeliveryRequest;
+use App\Services\Achievements\AudienceBrandAchievementService;
 
 class PrizeController extends BaseController
 {
@@ -38,18 +39,31 @@ class PrizeController extends BaseController
 
    
 
-    public function audienceBrandPrize(Request $request, Brand $brand)
+    public function achievements(Request $request, Brand $brand)
     {
         try {
             // dd($brand);
             
-            $data = (new GetBrandPrizeUserService($request, $brand->id))->run();
+            $data = (new AudienceBrandAchievementService($request, $brand->id))->run();
 
         }  catch (\Exception $e){
             return $this->sendError("something went wrong", ['error' => $e->getMessage()], 500);
         }        
         return $this->sendResponse($data, "Brand prize retrieved succcessfully");
     }
+
+    // public function audienceBrandPrize(Request $request, Brand $brand)
+    // {
+    //     try {
+    //         // dd($brand);
+            
+    //         $data = (new GetBrandPrizeUserService($request, $brand->id))->run();
+
+    //     }  catch (\Exception $e){
+    //         return $this->sendError("something went wrong", ['error' => $e->getMessage()], 500);
+    //     }        
+    //     return $this->sendResponse($data, "Brand prize retrieved succcessfully");
+    // }
 
 
     public function getAudiencePointBalance(Request $request, Brand $brand)
---
+++ app/Services/Achievement/AudienceBrandAchievementService.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Services\Achievements;
+
+use App\Models\BrandPoint;
+use Illuminate\Http\Request;
+use App\Models\AudienceBadge;
+use App\Models\BrandAudienceReward;
+use App\Services\BaseServiceInterface;
+use App\Services\Utility\GetAudienceRankService;
+use App\Services\Utility\GetAudienceBadgeListService;
+
+class AudienceBrandAchievementService implements BaseServiceInterface{
+    protected $brandId;
+    protected $request;
+
+    public function __construct(Request $request, $brandId)
+    {
+        $this->brandId = $brandId;
+        $this->request = $request;
+    }
+
+    public function run() {
+        $audienceId =  $this->request->user()->id;
+        $audiencReward = BrandAudienceReward::where('brand_id', $this->brandId)
+            ->where('audience_id', $audienceId)
+            ->with('prize')
+            ->get();
+
+
+        $audienceBadges = AudienceBadge::where('brand_id', $this->brandId)
+            ->where('audience_id', $audienceId)
+            ->get();
+
+
+        $audienceBrandPoint = BrandPoint::where('brand_id', $this->brandId)        
+            ->where("audience_id", $audienceId)
+            ->first();
+
+     
+        $points = $audienceBrandPoint->points ?? 0;
+
+        $audienceBadgesList = (new GetAudienceBadgeListService( $this->brandId, $audienceId, $points))->run();
+
+        $rank = (new GetAudienceRankService($this->brandId, $audienceId,))->run();
+        // if (!$prize) {
+        //     return []
+        // }
+
+        return ["point" => $points, "rank" => $rank, "badges" => $audienceBadgesList , "audience_reward" => $audiencReward, "audience_badges" => $audienceBadges];
+
+    }
+}
