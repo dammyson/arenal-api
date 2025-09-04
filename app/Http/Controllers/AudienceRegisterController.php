878344d43654ff932901c0f78a4bd36a8886ecc4
Emeka-techDev <gilbertgenye@gmail.com>
2025-09-04 10:19:07 +0100

    login issue

--- app/Http/Controllers/AudienceRegisterController.php
+++ app/Http/Controllers/AudienceRegisterController.php
@@ -122,6 +122,7 @@ class AudienceRegisterController extends BaseController
             $user = Audience::where('email', $request['email_or_phone'])
                 ->orWhere('phone_number', $request['email_or_phone'])
                 ->first();
+            dd($user);
 
             if (is_null($user)) {
                 return response()->json(['error' => true, 'message' => 'Invalid credentials'], 401);
