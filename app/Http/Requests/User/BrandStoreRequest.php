1ab378183df76211f97c5f0148ff0c7fd464f60c
Emeka-techDev <gilbertgenye@gmail.com>
2025-11-03 12:51:11 +0100

    remove required from store brand request

--- app/Http/Requests/User/BrandStoreRequest.php
+++ app/Http/Requests/User/BrandStoreRequest.php
@@ -32,7 +32,7 @@ class BrandStoreRequest extends FormRequest
             'client_id' => ['required', 'uuid', 'exists:Clients,id'],
             "brand_details" => "sometimes|array",
             "brand_details.*.brand_detail" => "sometimes|string",
-            "brand_branches" => "required|array",
+            "brand_branches" => "sometimes|array",
             "brand_branches.*.branch_name" => "required|string",
         ];
     }
